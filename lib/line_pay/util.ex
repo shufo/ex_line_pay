4a607300d6b5e590767305dad46c6d41887a0705
shufo <meikyowise@gmail.com>
2017-10-20 03:56:41 +0900

    Format Utils

--- lib/line_pay/util.ex
+++ lib/line_pay/util.ex
@@ -1,23 +1,25 @@
 defmodule LinePay.Util do
-  def datetime_from_timestamp(ts) when is_binary ts do
-    ts = case Integer.parse ts do
-      :error -> 0
-      {i, _r} -> i
-    end
-    datetime_from_timestamp ts
+  def datetime_from_timestamp(ts) when is_binary(ts) do
+    ts =
+      case Integer.parse(ts) do
+        :error -> 0
+        {i, _r} -> i
+      end
+
+    datetime_from_timestamp(ts)
   end
 
-  def datetime_from_timestamp(ts) when is_number ts do
-    {{year, month, day}, {hour, minutes, seconds}} = :calendar.gregorian_seconds_to_datetime ts
+  def datetime_from_timestamp(ts) when is_number(ts) do
+    {{year, month, day}, {hour, minutes, seconds}} = :calendar.gregorian_seconds_to_datetime(ts)
     {{year + 1970, month, day}, {hour, minutes, seconds}}
   end
 
   def datetime_from_timestamp(nil) do
-    datetime_from_timestamp 0
+    datetime_from_timestamp(0)
   end
 
   def string_map_to_atoms([string_key_map]) do
-    string_map_to_atoms string_key_map
+    string_map_to_atoms(string_key_map)
   end
 
   def string_map_to_atoms(string_key_map) do
@@ -27,8 +29,8 @@ defmodule LinePay.Util do
   def handle_line_pay_response(res) do
     cond do
       res["error"] -> {:error, res}
-      res["data"] -> {:ok, Enum.map(res["data"], &LinePay.Util.string_map_to_atoms &1)}
-      true -> {:ok, LinePay.Util.string_map_to_atoms res}
+      res["data"] -> {:ok, Enum.map(res["data"], &LinePay.Util.string_map_to_atoms(&1))}
+      true -> {:ok, LinePay.Util.string_map_to_atoms(res)}
     end
   end
 
@@ -37,31 +39,31 @@ defmodule LinePay.Util do
   def handle_line_pay_full_response(res) do
     cond do
       res["error"] -> {:error, res}
-      true -> {:ok, LinePay.Util.string_map_to_atoms res}
+      true -> {:ok, LinePay.Util.string_map_to_atoms(res)}
     end
   end
 
-  def list_raw( endpoint, opts \\ []) do
+  def list_raw(endpoint, opts \\ []) do
     defaults = [limit: 10, offset: nil, since: 0, until: nil]
     opts = Keyword.merge(defaults, opts)
-    list_raw endpoint, LinePay.config_or_env_key, opts
+    list_raw(endpoint, LinePay.config_or_env_key(), opts)
   end
 
-  def list_raw( endpoint, key, opts)  do
-    q = "#{endpoint}?#{opts |> Enum.filter(fn{_, v} -> v end) |> URI.encode_query}"
+  def list_raw(endpoint, key, opts) do
+    q = "#{endpoint}?#{opts |> Enum.filter(fn {_, v} -> v end) |> URI.encode_query()}"
 
-    LinePay.make_request_with_key(:get, q, key )
-    |> LinePay.Util.handle_line_pay_full_response
+    LinePay.make_request_with_key(:get, q, key)
+    |> LinePay.Util.handle_line_pay_full_response()
   end
 
-  def list( endpoint, key, opts) do
-    list_raw endpoint, key, opts
+  def list(endpoint, key, opts) do
+    list_raw(endpoint, key, opts)
   end
 
-  def list( endpoint, opts \\ []) do
+  def list(endpoint, opts \\ []) do
     defaults = [limit: 10, offset: nil, since: 0, until: nil]
     opts = Keyword.merge(defaults, opts)
 
-    list endpoint, LinePay.config_or_env_key, opts
+    list(endpoint, LinePay.config_or_env_key(), opts)
   end
 end
